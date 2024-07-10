662e8fd040d11384ec68344dfac85f6685532ce8
Tom Barthelmeh <tba105@uclive.ac.nz>
2024-07-10 17:34:44 +1200

    Added styling for basic administrator dashboard

--- app/(admin)/dashboard.tsx
+++ app/(admin)/dashboard.tsx
@@ -1,10 +1,16 @@
-import { Text, View } from 'react-native'
+import { Text, View, SafeAreaView } from 'react-native'
 
 const Dashboard = () => {
   return (
-    <View>
-      <Text>dashboard</Text>
-    </View>
+    <SafeAreaView className="bg-background h-full relative">
+      <View className="w-full h-full relative px-4">
+
+        <View className="flex justify-center items-center my-16">
+          <Text className="font-bold font-title text-3xl text-text">Admin Dashboard</Text>
+        </View>
+
+      </View>
+    </SafeAreaView>
   )
 }
 
